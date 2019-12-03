8dc6ba23252c4bdea51d43132c09d03355dbf406
Yarosky Cruz <yaro@Yaroskys-MacBook-Pro-2.local>
2019-12-02 20:33:11 -0500

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-# the50states
-Just a static site showing the 50 states.
-
-Created this site to start practicing and using CSS Grid. 
---
+++ precache-manifest.fa76e003d4df16419501a853cd2e96f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32ad5d44a6e73e79688ea00ca325748f",
+    "url": "/the50States/index.html"
+  },
+  {
+    "revision": "3bfe93b4c2ffa8d9ee45",
+    "url": "/the50States/static/css/main.6079eff2.chunk.css"
+  },
+  {
+    "revision": "b47de471494274a678e4",
+    "url": "/the50States/static/js/2.80e7718c.chunk.js"
+  },
+  {
+    "revision": "3bfe93b4c2ffa8d9ee45",
+    "url": "/the50States/static/js/main.c46e9c3e.chunk.js"
+  },
+  {
+    "revision": "13307baa32b7e82edee8",
+    "url": "/the50States/static/js/runtime-main.66af9c27.js"
+  }
+]);
